deec32e9c577dca419056daa6ea92e605702a8bb
tnbahia <tnbahia@gmail.com>
2019-04-17 20:24:45 +0100

    added spring-config, presentation and persistence.xml

---
+++ src/main/java/org/academiadecodigo/mainiacs/persistence/dao/ServiceDao.java
@@ -0,0 +1,13 @@
+package org.academiadecodigo.mainiacs.persistence.dao;
+
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Repository
+public class ServiceDao {
+
+    @PersistenceContext
+    private EntityManager em;
+}
--- src/main/java/org/academiadecodigo/mainiacs/services/UserService.java
+++ src/main/java/org/academiadecodigo/mainiacs/services/UserService.java
@@ -1,13 +1,29 @@
 package org.academiadecodigo.mainiacs.services;
 
-import org.academiadecodigo.mainiacs.models.Service;
+import org.academiadecodigo.mainiacs.persistence.models.Service;
 
+import java.util.Date;
 import java.util.LinkedList;
 import java.util.List;
 
 public class UserService {
     public List<Service> listServices() {
         List<Service> serviceList = new LinkedList<>();
+        Service s = new Service("Tiago",
+                "ojicois",
+                "09809123",
+                new Date(2019,1,23),
+                new Date(2019,2,26),
+                "Porto");
+        Service t = new Service("Bahia",
+                "sdfs",
+                "12312332",
+                new Date(2019,4,23),
+                new Date(2019,7,26),
+                "Porto");
+
+        serviceList.add(s);
+        serviceList.add(t);
         return serviceList;
     }
 }
