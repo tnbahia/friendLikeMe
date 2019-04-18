5b1700373b81366f0e139807bb692abf940dafea
tnbahia <tnbahia@gmail.com>
2019-04-18 11:05:01 +0100

    changed comments message

--- src/main/java/org/academiadecodigo/mainiacs/persistence/dao/OfferDao.java
+++ src/main/java/org/academiadecodigo/mainiacs/persistence/dao/OfferDao.java
@@ -11,11 +11,12 @@ import java.util.List;
 @Component
 @Repository
 public class OfferDao {
-    public OfferDao() {}
+    public OfferDao() {
+    }
 
     private List<Offer> list = new LinkedList<>();
 
-    public void add (Offer offer) {
+    public void add(Offer offer) {
         System.out.println("addingOfferDAO\n\n\n\n\n\n\n\n\n\n\n");
         System.out.println("List size " + list.size());
         list.add(offer);
@@ -24,39 +25,41 @@ public class OfferDao {
 
     public List<Offer> getList() {
         System.out.println("List size " + list.size());
-        Offer a = new Offer("Tiago",
-                "ojicois",
-                "09809123",
-                40,
-                "sa",
-                "asd",
-                "Porto");
-        Offer b = new Offer("Bahia",
-                "sdfs",
-                "12312332",
-                30,
-                "sa",
-                "asd",
-                "Porto");
-        Offer c = new Offer("Mila",
-                "sdfs",
-                "12312332",
-                30,
-                "sa",
-                "asd",
-                "Porto");
-        Offer d = new Offer("Fábio",
-                "sdfs",
-                "12312332",
-                30,
-                "sa",
-                "asd",
-                "Porto");
+        if (list.isEmpty()) {
+            Offer a = new Offer("Tiago",
+                    "tiago@incrivel.com",
+                    "123456789",
+                    40,
+                    "03-05-2019",
+                    "10-05-2019",
+                    "Porto");
+            Offer b = new Offer("Bahia",
+                    "bahia@top.com",
+                    "12312332",
+                    35,
+                    "10-06-2019",
+                    "10-07-2019",
+                    "Porto");
+            Offer c = new Offer("Mila",
+                    "mila@fabulosa.com",
+                    "12312332",
+                    38,
+                    "16-06-2019",
+                    "30-08-2019",
+                    "Porto");
+            Offer d = new Offer("Fábio",
+                    "fabio@theman.com",
+                    "12312332",
+                    40,
+                    "20-06-2019",
+                    "25-07-2019",
+                    "Porto");
 
-        list.add(a);
-        list.add(b);
-        list.add(c);
-        list.add(d);
+            list.add(a);
+            list.add(b);
+            list.add(c);
+            list.add(d);
+        }
         return list;
     }
 
