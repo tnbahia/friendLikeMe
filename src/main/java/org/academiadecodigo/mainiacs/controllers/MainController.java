6ac8b745e52b97459124d3809c04df7d53fd4138
tnbahia <tnbahia@gmail.com>
2019-04-18 02:47:12 +0100

    tried to serve css

---
+++ src/main/java/org/academiadecodigo/mainiacs/controllers/MainController.java
@@ -0,0 +1,17 @@
+package org.academiadecodigo.mainiacs.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@CrossOrigin(origins = "*")
+@Controller
+public class MainController {
+
+    @RequestMapping(method = RequestMethod.GET, path = {"/",""})
+    public String home() {
+        System.out.println("ochja");
+        return "test";
+    }
+}
--- src/main/java/org/academiadecodigo/mainiacs/persistence/dao/OfferDao.java
+++ src/main/java/org/academiadecodigo/mainiacs/persistence/dao/OfferDao.java
@@ -3,16 +3,10 @@ package org.academiadecodigo.mainiacs.persistence.dao;
 import org.academiadecodigo.mainiacs.persistence.models.Offer;
 import org.springframework.stereotype.Repository;
 
-
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
 import java.util.List;
 
 @Repository
 public class OfferDao {
-
-    @PersistenceContext
-    private EntityManager em;
 
     private List<Offer> list;
 
